6350bc730e10ca57e23886f0b8aaac1c4f95a06f
pokutuna <popopopopokutuna@gmail.com>
2014-12-06 12:48:56 +0900

    after the decimal point

--- scripts/storage_capacity.rb
+++ scripts/storage_capacity.rb
@@ -21,6 +21,7 @@ if ENV['MACKEREL_AGENT_PLUGIN_META'] == '1'
   exit 0
 end
 
-used = `df . | grep '/' | awk '{ print $5 }' | sed 's/%//'`.strip.to_i
-puts [ 'storage.used',            used, Time.now.to_i ].join("\t")
-puts [ 'storage.available', 100 - used, Time.now.to_i ].join("\t")
+df = `df -k . | grep '/' | awk '{ print $2 " " $3 }'`.strip.split(' ').map(&:to_i)
+size, used = *df
+puts [ 'storage.used',               used / size.to_f * 100, Time.now.to_i ].join("\t")
+puts [ 'storage.available', (size - used) / size.to_f * 100, Time.now.to_i ].join("\t")
